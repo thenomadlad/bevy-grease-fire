75cb672f207124cb41736b1f0ff92370b8d9545f
Aditya Dhara <dev@adityadhara.com>
2026-05-30 03:08:18 -0700

    Credits

--- src/menus/credits.rs
+++ src/menus/credits.rs
@@ -31,15 +31,11 @@ fn spawn_credits_menu(mut commands: Commands) {
 }
 
 fn created_by() -> impl Bundle {
-    grid(vec![
-        ["Joe Shmoe", "Implemented alligator wrestling AI"],
-        ["Jane Doe", "Made the music for the alien invasion"],
-    ])
+    grid(vec![["thenomadlad", "(Aditya Dhara, if you must know)"]])
 }
 
 fn assets() -> impl Bundle {
     grid(vec![
-        ["Ducky sprite", "CC0 by Caz Creates Games"],
         ["Button SFX", "CC0 by Jaszunio15"],
         ["Music", "CC BY 3.0 by Kevin MacLeod"],
         [
