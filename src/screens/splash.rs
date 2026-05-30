6b8eb61072fe3c71e299d7103a019b9caf15e46e
Aditya Dhara <dev@adityadhara.com>
2026-05-30 03:14:50 -0700

    Removed ducky

--- src/screens/splash.rs
+++ src/screens/splash.rs
@@ -1,29 +1,13 @@
 //! A splash screen that plays briefly at startup.
 
-use bevy::{
-    image::{ImageLoaderSettings, ImageSampler},
-    input::common_conditions::input_just_pressed,
-    prelude::*,
-};
+use bevy::{input::common_conditions::input_just_pressed, prelude::*};
 
 use crate::{AppSystems, screens::Screen, theme::prelude::*};
 
 pub(super) fn plugin(app: &mut App) {
-    // Spawn splash screen.
     app.insert_resource(ClearColor(SPLASH_BACKGROUND_COLOR));
     app.add_systems(OnEnter(Screen::Splash), spawn_splash_screen);
 
-    // Animate splash screen.
-    app.add_systems(
-        Update,
-        (
-            tick_fade_in_out.in_set(AppSystems::Computations),
-            apply_fade_in_out.in_set(AppSystems::Update),
-        )
-            .run_if(in_state(Screen::Splash)),
-    );
-
-    // Add splash timer.
     app.add_systems(OnEnter(Screen::Splash), insert_splash_timer);
     app.add_systems(OnExit(Screen::Splash), remove_splash_timer);
     app.add_systems(
@@ -35,7 +19,6 @@ pub(super) fn plugin(app: &mut App) {
             .run_if(in_state(Screen::Splash)),
     );
 
-    // Exit the splash screen early if the player hits escape.
     app.add_systems(
         Update,
         enter_title_screen
@@ -45,71 +28,13 @@ pub(super) fn plugin(app: &mut App) {
 
 const SPLASH_BACKGROUND_COLOR: Color = Color::srgb(0.157, 0.157, 0.157);
 const SPLASH_DURATION_SECS: f32 = 1.8;
-const SPLASH_FADE_DURATION_SECS: f32 = 0.6;
 
-fn spawn_splash_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn spawn_splash_screen(mut commands: Commands) {
     commands.spawn((
         widget::ui_root("Splash Screen"),
         BackgroundColor(SPLASH_BACKGROUND_COLOR),
         DespawnOnExit(Screen::Splash),
-        children![(
-            Name::new("Splash image"),
-            Node {
-                margin: UiRect::all(Val::Auto),
-                width: percent(70),
-                ..default()
-            },
-            ImageNode::new(asset_server.load_with_settings(
-                // This should be an embedded asset for instant loading, but that is
-                // currently [broken on Windows Wasm builds](https://github.com/bevyengine/bevy/issues/14246).
-                "images/splash.png",
-                |settings: &mut ImageLoaderSettings| {
-                    // Make an exception for the splash image in case
-                    // `ImagePlugin::default_nearest()` is used for pixel art.
-                    settings.sampler = ImageSampler::linear();
-                },
-            )),
-            ImageNodeFadeInOut {
-                total_duration: SPLASH_DURATION_SECS,
-                fade_duration: SPLASH_FADE_DURATION_SECS,
-                t: 0.0,
-            },
-        )],
     ));
-}
-
-#[derive(Component, Reflect)]
-#[reflect(Component)]
-struct ImageNodeFadeInOut {
-    /// Total duration in seconds.
-    total_duration: f32,
-    /// Fade duration in seconds.
-    fade_duration: f32,
-    /// Current progress in seconds, between 0 and [`Self::total_duration`].
-    t: f32,
-}
-
-impl ImageNodeFadeInOut {
-    fn alpha(&self) -> f32 {
-        // Normalize by duration.
-        let t = (self.t / self.total_duration).clamp(0.0, 1.0);
-        let fade = self.fade_duration / self.total_duration;
-
-        // Regular trapezoid-shaped graph, flat at the top with alpha = 1.0.
-        ((1.0 - (2.0 * t - 1.0).abs()) / fade).min(1.0)
-    }
-}
-
-fn tick_fade_in_out(time: Res<Time>, mut animation_query: Query<&mut ImageNodeFadeInOut>) {
-    for mut anim in &mut animation_query {
-        anim.t += time.delta_secs();
-    }
-}
-
-fn apply_fade_in_out(mut animation_query: Query<(&ImageNodeFadeInOut, &mut ImageNode)>) {
-    for (anim, mut image) in &mut animation_query {
-        image.color.set_alpha(anim.alpha())
-    }
 }
 
 #[derive(Resource, Debug, Clone, PartialEq, Reflect)]
